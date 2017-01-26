c04e30f2fb79ffb2de60d0944425aa68d787c25b
Balazs Brinkus <balazs@brinkus.com>
2017-01-26 12:32:53 +0100

    labs-cloud-starter-service: add configuration condition to disable the Eureka Client usage

--- labs-cloud-starters/labs-cloud-starter-service/src/main/java/com/brinkus/labs/cloud/service/config/EurekaClientConfig.java
+++ labs-cloud-starters/labs-cloud-starter-service/src/main/java/com/brinkus/labs/cloud/service/config/EurekaClientConfig.java
@@ -32,6 +32,7 @@ import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestOperations;
@@ -42,6 +43,7 @@ import java.net.UnknownHostException;
 
 @Configuration
 @EnableEurekaClient
+@Conditional(EurekaClientConfigCondition.class)
 public class EurekaClientConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EurekaClientConfig.class);
